7b67859d18100a42432af56d87f402cc52f6e34f
zehraxaa <129674249+zehraxaa@users.noreply.github.com>
2024-02-19 22:13:17 +0300

    Update LibraryMS.py

--- LibraryMS.py
+++ LibraryMS.py
@@ -1,7 +1,6 @@
 class Library:
-    def __init__(self, filename='books.txt'):
-        self.filename = filename
-        self.file = open(self.filename, 'a+')
+    def __init__(self):
+        self.file = open("books.txt", "a+")
 
     def __del__(self):
         self.file.close()
@@ -11,50 +10,48 @@ class Library:
         lines = self.file.read().splitlines()
         for line in lines:
             book_info = line.split(',')
-            print(f"Book: {book_info[0]}, Author: {book_info[1]}")
+            book_title = book_info[0]
+            book_author = book_info[1]
+            print(f"Book: {book_title}\nAuthor: {book_author}\n")
 
     def add_book(self):
-        title = input("Enter book title: ")
-        author = input("Enter author name: ")
-        release_date = input("Enter release year: ")
+        book_title = input("Enter book title: ")
+        book_author = input("Enter book author: ")
+        release_year = input("Enter release year: ")
         num_pages = input("Enter number of pages: ")
-
-        book_info = f"{title} ,{author} ,{release_date} ,{num_pages}\n"
+        book_info = f"{book_title},{book_author},{release_year},{num_pages}\n"
         self.file.write(book_info)
-        print("Book added successfully.")
+        print("Book added successfully!")
 
     def remove_book(self):
-        title_to_remove = input("Enter the title of the book to remove: ")
-
-        lines = self.file.readlines()
+        book_title = input("Enter the title of the book to remove: ")
+        self.file.seek(0)
+        lines = self.file.read().splitlines()
+        updated_books = []
+        for line in lines:
+            book_info = line.split(',')
+            if book_info[0] != book_title:
+                updated_books.append(line)
         self.file.seek(0)
         self.file.truncate()
+        for book in updated_books:
+            self.file.write(book + '\n')
+        print("Book removed successfully!")
 
-        for line in lines:
-            if title_to_remove not in line:
-                self.file.write(line)
-
-        print("Book removed successfully.")
 
 lib = Library()
 
 while True:
-    print("\n*** MENU ***")
+    print("\n*** MENU **")
     print("1) List Books")
     print("2) Add Book")
     print("3) Remove Book")
-    print("4) Exit")
-
-    user_input = input("Enter your choice (1-4): ")
-
-    if user_input == '1':
+    choice = input("Enter your choice (1-3): ")
+    if choice == "1":
         lib.list_books()
-    elif user_input == '2':
+    elif choice == "2":
         lib.add_book()
-    elif user_input == '3':
+    elif choice == "3":
         lib.remove_book()
-    elif user_input == '4':
-        print("Exiting the program. Goodbye!")
-        break
     else:
-        print("Invalid choice. Please enter a number between 1 and 4.")
+        print("Invalid choice. Please try again.")
